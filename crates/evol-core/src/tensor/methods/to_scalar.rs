299d027fab1eab072beb4621998d5169918f9f34
xupremix <xupremix.me@gmail.com>
2024-12-10 11:16:44 +0100

    fix unconstrained type

--- crates/evol-core/src/tensor/methods/to_scalar.rs
+++ crates/evol-core/src/tensor/methods/to_scalar.rs
@@ -4,9 +4,9 @@ use crate::{
     tensor::{Device, Kind, Shape, Tensor},
 };
 
-impl<S: ToScalar, K: Kind, D: Device> Tensor<Scalar, K, D> {
+impl<K: Kind, D: Device> Tensor<Scalar, K, D> {
     pub fn to_scalar(&self) -> K {
-        S::TO_SCALAR_CHECK;
+        Scalar::TO_SCALAR_CHECK;
         self.repr.to_scalar().unwrap()
     }
 }

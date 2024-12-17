f7df0fcd5453b81326b5054d3ce29c5e62bc7c04
xupremix <xupremix.me@gmail.com>
2024-12-17 17:54:43 +0100

    save

--- examples/tensor/src/main.rs
+++ examples/tensor/src/main.rs
@@ -1,6 +1,6 @@
-#![allow(incomplete_features)]
-#![feature(generic_const_exprs)]
-
+// #![allow(incomplete_features)]
+// #![feature(generic_const_exprs)]
+//
 use evol::prelude::*;
 
 type Custom = (Linear<784, 20>, Relu, Linear<20, 30>, Softmax);
@@ -8,8 +8,12 @@ type Custom = (Linear<784, 20>, Relu, Linear<20, 30>, Softmax);
 fn main() {
     let vm = VarMap::new();
     let vs: Vs = Vs::from_varmap(&vm);
+    println!("Loading MNIST dataset...");
     let dt = DataLoader::<Cuda>::mnist();
+    println!("Building model...");
     let model = Custom::build(&vs, Default::default());
+    println!("Forwarding model...");
     let xs = model.forward(dt.train_images());
+    println!("Model output:");
     println!("{:?}", xs);
 }

6f44ee944636f524a338363a238ccc2b39bb6890
xupremix <xupremix.me@gmail.com>
2024-12-28 15:55:45 +0100

    seq not supporting type different than f32

--- examples/tensor/src/main.rs
+++ examples/tensor/src/main.rs
@@ -1,19 +1,20 @@
-// #![allow(incomplete_features)]
-// #![feature(generic_const_exprs)]
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
 
 use evol::prelude::*;
 
-type Custom = (Linear<784, 20>, Relu, Linear<20, 2>, Swiglu);
+// type MnistModel = (Linear<784, 20>, Relu, Linear<20, 2>, Swiglu);
+type CifarModel = (Linear<32, 20>, Relu, Linear<20, 2>, Swiglu);
 
 fn main() {
     let vm = VarMap::new();
     let vs: Vs = Vs::from_varmap(&vm);
-    println!("Loading MNIST dataset...");
-    let dt = DataLoader::<Cuda>::mnist();
+    println!("Loading CIFAR dataset...");
+    let dt = DataLoader::<Cpu>::cifar();
     println!("Building model...");
-    let model = Custom::build(&vs, Default::default());
+    let model = CifarModel::build(&vs, Default::default());
     println!("Forwarding model...");
-    let xs = model.forward(dt.train_images());
+    let xs = model.forward(&dt.train_images().to_kind::<f32>());
     println!("Model output:");
     println!("{:?}", xs);
 }

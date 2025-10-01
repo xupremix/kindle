c4c63e51a7cbbe73db3924460a0385009d25179c
xupremix <xupremix.me@gmail.com>
2025-10-01 12:53:03 +0200

    changed training batch size + forward

--- examples/mnist/src/main.rs
+++ examples/mnist/src/main.rs
@@ -4,13 +4,18 @@
 use kindle::{nn::optim::Backward, prelude::*};
 
 dataset! {
-    MnistDataset,
+    MnistTrainDataset,
     "/home/xupremix/Desktop/train.parquet"
 }
 
+dataset! {
+    MnistTestDataset,
+    "/home/xupremix/Desktop/test.parquet"
+}
+
 const LR: f64 = 0.1;
-const BATCH_SIZE: usize = 125;
-const EPOCHS: usize = 15;
+const BATCH_SIZE: usize = 30;
+const EPOCHS: usize = 3;
 const N_SAMPLES: usize = 60_000;
 
 struct MnistModel {
@@ -44,19 +49,19 @@ impl MnistModel {
             .forward(&xs)
             .flatten_from::<1, Rank2<_, _>>()
             .dropout(0.5);
-        self.fc.forward(&xs).softmax::<1>()
+        self.fc.forward(&xs)
     }
 }
 
 fn main() {
-    let dataset: MnistDataset = MnistDataset::load().unwrap();
-    let tensors = dataset.images.unsqueeze::<1>().to_kind::<f32>();
+    let train_dataset: MnistTrainDataset = MnistTrainDataset::load().unwrap();
+    let tensors = train_dataset.images.unsqueeze::<1>().to_kind::<f32>();
 
     let x_train = &tensors.chunk::<0, BATCH_SIZE>();
-    let y_train = &dataset.labels.to_kind::<u32>().chunk::<0, BATCH_SIZE>();
-
-    // let x_test = x_train.pop();
-    // let y_test = y_train.pop();
+    let y_train = &train_dataset
+        .labels
+        .to_kind::<u32>()
+        .chunk::<0, BATCH_SIZE>();
 
     let vm = VarMap::new();
     let vs: Vs = Vs::from_varmap(&vm);
@@ -85,5 +90,16 @@ fn main() {
         println!("epoch {epoch}: loss: {total_loss:.4}, accuracy: {acc:.4}")
     }
 
-    vm.save("./model_save.safetensors").unwrap();
+    // Testing with the trained model
+    let test_dataset: MnistTestDataset = MnistTestDataset::load().unwrap();
+    let samples = test_dataset.images.shape()[0];
+    let x_test = test_dataset.images.unsqueeze::<1>().to_kind::<f32>();
+    let y_test = test_dataset.labels.to_kind::<u32>();
+
+    let preds = model.forward(&x_test).argmax::<1>();
+    let correct = preds.eq(&y_test).to_kind::<u32>().sum_all().to_scalar();
+    let accuracy = correct as f32 / samples as f32;
+    println!("Accuracy on test set: {accuracy:.4}");
+
+    vm.save(format!("mnist{accuracy:.0}.safetensors")).unwrap();
 }
